3e49dd94e193a4676ab916307e1c8822a41a6060
totskaiasonia <totskaiasonia@gmail.com>
2021-07-30 15:21:14 +0300

    init commit

---
+++ Game/Game/Game.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include<SFML/Graphics.hpp>
+
+using namespace sf;
+
+int main()
+{
+    RenderWindow window(VideoMode(800, 800), "Tanks");
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+            {
+                window.close();
+            }
+        }
+        window.display();
+    }
+}
